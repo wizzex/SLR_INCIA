058b71569faaecfae3970b2c89e09cb900dc0ea2
wizzex <luca7lemarchand@gmail.com>
2025-06-05 16:45:44 +0200

    2 fibres intra Mileusnic

--- MyProj/TestMileusnic.py
+++ MyProj/TestMileusnic.py
@@ -3,9 +3,7 @@ from MileusnicSpindle import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt = 0.0002  # s
-time = np.arange(0, 2, dt)
-stretch = np.zeros_like(time)
+
 """
 ----------------------------------------------------------------------------------------
 Création du mouvement en pourcentage L0 de 0.95 à 0.108 sur 20ms
@@ -16,17 +14,60 @@ Création du mouvement en pourcentage L0 de 0.95 à 0.108 sur 20ms
 # start_value = 0.76
 # end_value = 0.82
 """
+
+
+dt = 0.00002  # s
+time = np.arange(0, 2, dt)
 start_value = 0.95
 end_value = 1.08
 ramp_start = 1  # s
 ramp_end = 1.2  # s
 ramp_duration = ramp_end - ramp_start
 ramp_slope = (end_value - start_value) / ramp_duration
+L = np.zeros_like(time)
+for i, t in enumerate(time):
+    if t <= ramp_start:
+        L[i] = start_value
+    elif t > ramp_start and t < ramp_end:
+        L[i] = start_value + ramp_slope * (t - ramp_start)
+    else:
+        L[i] = end_value
+dL = np.gradient(L, dt)
+d2L = np.gradient(dL, dt)
 
 """
----------------------------------------------------------------------------
-# Création d'un fuseau pour changer valeur de stimulation gamma -> gamma_freq entre 0 et 100 (en firing units)
----------------------------------------------------------------------------
+dt = 0.0002  # s
+time = np.arange(0, 5, dt)
+start_value = 0.95
+end_value = 1.08
+ramp_slope = (end_value - start_value) / 0.2
+L = np.zeros_like(time)
+for i, t in enumerate(time):
+    if t <= 1:
+        L[i] = start_value
+    elif t > 1 and t <= 1.2:
+        L[i] = start_value + ramp_slope * (t - 1)
+    elif t > 1.2 and t < 2:
+        L[i] = end_value
+    elif t >= 2 and t <= 2.2:
+        L[i] = end_value - ramp_slope * (t - 2)
+    elif t > 2.2 and t < 3:
+        L[i] = start_value
+    elif t >= 3 and t <= 3.2:
+        L[i] = start_value + ramp_slope * (t - 3)
+    elif t > 3.2 and t < 4:
+        L[i] = end_value
+    elif t >= 4 and t <= 4.2:
+        L[i] = end_value - ramp_slope * (t - 4)
+    else:
+        L[i] = start_value
+dL = np.gradient(L, dt)
+d2L = np.gradient(dL, dt)
+"""
+"""
+----------------------------------------------------------------------------------------------------------------------------
+ # Création d'un fuseau pour changer valeur de stimulation gamma -> gamma_freq entre 0 et 100 (en firing units)
+----------------------------------------------------------------------------------------------------------------------------
 """
 DynFiber = MileusnicIntrafusal(
     Ksr=10.4649,
@@ -45,7 +86,7 @@ DynFiber = MileusnicIntrafusal(
     C_shortening=0.42,
     C_lengthening=1,
     a=0.3,
-    gamma_freq=60,
+    gamma_freq=0,
     freq_to_activation=60,
     dt=dt,
     p=2,
@@ -68,7 +109,7 @@ StatFiber = MileusnicIntrafusal(
     C_shortening=0.42,
     C_lengthening=1,
     a=0.3,
-    gamma_freq=60,
+    gamma_freq=100,
     freq_to_activation=60,
     dt=dt,
     p=2,
@@ -80,20 +121,17 @@ IaStat = []
 IaDyn = []
 gammaActivation = []
 dersecondT = []
-TT = []
-LL = []
-dTT = []
-dLL = []
-d2LL = []
 niv_ac_gamma_dyn = []
 niv_ac_gamma_stat = []
+TT = []
 d2TT = []
 """
 ----------------------------------------------------------------------------------------------------
                             SIMULATION
---------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
 """
 Ia_output = []
+"""
 for t in time:
     if t < ramp_start:
         L = start_value
@@ -130,10 +168,27 @@ for t in time:
     niv_ac_gamma_dyn.append(DynFiber.f_gamma)
     niv_ac_gamma_stat.append(StatFiber.f_gamma)
     d2TT.append(DynFiber.d2T)
+"""
+for i, t in enumerate(time):
+    StatFiber.update(L[i], dt, dL[i], d2L[i])
+    DynFiber.update(L[i], dt, dL[i], d2L[i])
+    Spindle.update(0.156)
+    IaStat.append(StatFiber.Ia_contrib)
+    IaDyn.append(DynFiber.Ia_contrib)
+    Ia_output.append(Spindle.Ia)
+    gammaActivation.append(DynFiber.f_gamma)
+    dersecondT.append(DynFiber.d2T)
+
+    TT.append(StatFiber.dT)
+
+    niv_ac_gamma_dyn.append(DynFiber.f_gamma)
+    niv_ac_gamma_stat.append(StatFiber.f_gamma)
+    d2TT.append(StatFiber.d2T)
+
 
 """
 -----------------------------------------------------------------
-AFFICHAGE 9 subplots
+                      AFFICHAGE 9 subplots
 ------------------------------------------------------------------
 """
 fig, axs = plt.subplots(3, 3, figsize=(10, 8))
@@ -141,14 +196,14 @@ ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9 = axs.flatten()
 
 # Graphe du stretch
 ax1.plot(time, Ia_output, label="IaOutput", color="blue")
-ax1.set_ylabel("Stretch")
-ax1.set_title("Ramp and Hold Stretch")
+ax1.set_ylabel("Firing unit")
+ax1.set_title("")
 ax1.set_xlim(0, 2)
 ax1.grid(True)
 ax1.legend()
 
 # Graphe de la réponse Ia
-ax2.plot(time, LL, label="L", color="green")
+ax2.plot(time, L, label="L", color="green")
 ax2.set_xlabel("Time (s)")
 ax2.set_xlim(0, 2)
 ax2.set_ylabel("LONGUEUR")
@@ -176,14 +231,14 @@ ax5.set_ylabel("Ia Output")
 ax5.grid(True)
 ax5.legend()
 
-ax6.plot(time, dLL, label="dl", color="green")
+ax6.plot(time, dL, label="dl", color="green")
 ax6.set_xlabel("Time (ms)")
 ax6.set_xlim(0, 2)
 ax6.set_ylabel("Ia Output")
 ax6.grid(True)
 ax6.legend()
 
-ax7.plot(time, d2LL, label="d2L", color="green")
+ax7.plot(time, d2L, label="d2L", color="green")
 ax7.set_xlabel("Time (ms)")
 ax7.set_xlim(0, 2)
 ax7.set_ylabel("Ia Output")
